66449c7a337dfa84b29a630d8dba0b6589ad6b68
Muntasir-Pranto <fahimmuntasir43@gmail.com>
2023-07-06 15:01:23 +0600

    Database redesign done

--- database/migrations/2023_06_07_061556_create_services_table.php
+++ database/migrations/2023_06_07_061556_create_services_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('image');
             $table->string('title');
+            $table->string('title_bn');
             $table->timestamps();
         });
     }
