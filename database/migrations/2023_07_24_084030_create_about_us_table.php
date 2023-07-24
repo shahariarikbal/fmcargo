9d60ff49a26f8cbac219549a72c17d996175ef5f
Muntasir2762 <muntasirpranto2762@gmail.com>
2023-07-24 14:52:34 +0600

    Database and seeder done for static pages

---
+++ database/migrations/2023_07_24_084030_create_about_us_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('about_us', function (Blueprint $table) {
+            $table->id();
+            $table->text('title_en');
+            $table->text('title_bn');
+            $table->longText('description_en');
+            $table->longText('description_bn');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('about_us');
+    }
+};
